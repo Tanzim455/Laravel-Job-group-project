cffe8ffe4f323420ab89364b9f54d391ff133c50
sayful <mdsayfulislam1411@gmail.com>
2024-02-13 16:55:13 +0600

    update user model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone_number',
+        'qualification',
+        'years_of_experience',
+        'cv'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('phone_number');
+            $table->string('qualification');
+            $table->integer('years_of_experience');
+            $table->string('cv');
             $table->rememberToken();
             $table->timestamps();
         });
