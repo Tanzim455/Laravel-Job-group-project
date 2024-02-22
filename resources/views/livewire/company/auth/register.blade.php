cbcd7f1484b62b4630014ade0e3d067849b3a0b1
sayful <mdsayfulislam1411@gmail.com>
2024-02-22 20:41:32 +0600

    changed session message and showing create button conditionally

--- resources/views/livewire/company/auth/register.blade.php
+++ resources/views/livewire/company/auth/register.blade.php
@@ -53,8 +53,7 @@
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password" wire:model="password"
                                 placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            >
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             @error('password')
                                 <span class="text-red-500">{{ $message }}</span>
                             @enderror
@@ -65,20 +64,24 @@
                                 password</label>
                             <input type="password" name="password_confirmation" id="password_confirmation"
                                 wire:model="password_confirmation" placeholder="••••••••"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            >
+                                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             @error('password_confirmation')
                                 <span class="text-red-500">{{ $message }}</span>
                             @enderror
                         </div>
-                        <button wire:click.prevent="register()" type="submit"
-                            class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+
+                        <button wire:click.prevent="register()" wire:loading.attr="disabled" type="submit"
+                            class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" wire:loading.remove>
                             Create an account
-                            {{-- <span wire:loading>Creating...</span> --}}
                         </button>
+                        <button disabled wire:loading wire:target="register()" class="w-full text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                            Creating...
+                        </button>
+
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                             Already have an account? <a href="{{ route('company.login') }}"
-                                class="font-medium text-primary-600 hover:underline dark:text-primary-500" wire:navigate>Login
+                                class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                wire:navigate>Login
                                 here</a>
                         </p>
                     </form>
