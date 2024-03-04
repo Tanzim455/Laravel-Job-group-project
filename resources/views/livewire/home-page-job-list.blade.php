5ee3b62995c2981d53c12c443c20b03c0cf5c31e
Tanzim455 <tanzim.ibthesam@gmail.com>
2024-03-04 22:22:57 +0600

    Css fixed of home page

--- resources/views/livewire/home-page-job-list.blade.php
+++ resources/views/livewire/home-page-job-list.blade.php
@@ -1,14 +1,14 @@
 <div>
     @foreach ($jobs as $job)
     <div class="m-5">
-        <div class="group mx-2 mt-10 grid max-w-screen-md grid-cols-12 space-x-8 overflow-hidden rounded-lg border py-8 text-gray-700 shadow transition hover:shadow-lg sm:mx-auto">
+        <div class="group mx-2 mt-10 grid max-w-screen-md grid-cols-12 space-x-8 overflow-hidden rounded-lg border py-6 text-gray-700 shadow transition hover:shadow-lg sm:mx-auto space-y-1">
           
           <div class="col-span-11 flex flex-col pr-8 text-left sm:pl-4">
             <h3 class="text-sm text-gray-600"></h3>
             <a href="#" class="mb-3 overflow-hidden pr-7 text-lg font-semibold sm:text-xl">{{$job->title}}</a>
             
       
-            <div class="mt-5 flex flex-col space-y-3 text-sm font-medium text-gray-500 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2 space-x-2">
+            <div class=" flex flex-col space-y-3 text-sm font-medium text-gray-500 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2 space-x-2">
               <div class="">Experience:<span class="ml-2 mr-3 rounded-full bg-green-100 px-2 py-0.5 text-green-900">{{$job->min_experience}}-{{$job->max_experience}} Years</span></div>
               <div class="">Salary:<span class="ml-2 mr-3 rounded-full bg-blue-100 px-2 py-0.5 text-blue-900">{{$job->min_salary}}-{{$job->max_salary}}</span>BDT</div>
               <div class="pl-2">Qualification<span class="ml-2 mr-3 rounded-full bg-blue-100 px-2 py-0.5 text-blue-900">{{$job->qualification}}</div>
@@ -24,6 +24,9 @@
     <div class="mt-5 flex flex-col space-y-3 text-sm font-medium text-gray-500 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2 space-x-2">
           CompanyName-{{$job->company_name}}
           CategoryName-{{$job->category_name}}
+    </div>
+  </div>
+</div>
     </div>
     @endforeach
 </div>
