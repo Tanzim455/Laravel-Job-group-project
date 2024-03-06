8025a37927878e517b9822e2314a2b08ed3ed22e
Tanzim455 <tanzim.ibthesam@gmail.com>
2024-03-06 06:43:44 +0600

    Spa navigation in frontend

--- resources/views/homenav.blade.php
+++ resources/views/homenav.blade.php
@@ -25,9 +25,15 @@
               </li>
               @endauth
               @guest
-              <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-              <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register as User</a>
-              <a href="{{ route('company.register') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register as Company</a>
+              <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+              wire:navigate
+              >Log in</a>
+              <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+              wire:navigate
+              >Register as User</a>
+              <a href="{{ route('company.register') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+              wire:navigate
+              >Register as Company</a>
               @endguest
                
               
